d05e581a61a5a77227042b7e40b7f40ac65b5b29
y24cd051 <y24cd051@rvrjc.ac.in>
2026-07-24 10:45:08 +0530

    Create longest_substring_without_reapeating_characters.py

---
+++ strings/longest_substring_without_reapeating_characters.py
@@ -0,0 +1,13 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        ans=set()
+        max_length=0
+        left=0
+        for right in range(len(s)):
+            while s[right] in ans:
+                ans.remove(s[left])
+                left+=1 
+            ans.add(s[right])
+            max_length=max(max_length,right-left+1)
+        return max_length
+
