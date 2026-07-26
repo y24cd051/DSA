b9941a8b21a33c9dc2c390372b6055051458649f
y24cd051 <y24cd051@rvrjc.ac.in>
2026-07-26 18:31:01 +0530

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.py
@@ -0,0 +1,6 @@
+import json
+class Solution:
+    def encode(self, strs: List[str]) -> str:
+        return json.dumps(strs)
+    def decode(self, s: str) -> List[str]:
+        return json.loads(s)
